91064eb36049bc566d9315732fe178c1a85fcc73
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-12-23 15:47:54 +0700

    second commit

--- app1.py
+++ app1.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# HALAMAN 1 (FORM)
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -10,7 +9,6 @@ def index():
         return redirect(url_for("hasil", nama=nama))
     return render_template("index.html")
 
-# HALAMAN 2 (HASIL)
 @app.route("/hasil")
 def hasil():
     nama = request.args.get("nama")
